1df510b0e516b5d49f4d57ddcdb62004e498d345
Joseph Carpenter <carpenter.joseph1998@gmail.com>
2022-05-26 17:25:04 +0200

    more styling in form builder

--- app/helpers/application_form.rb
+++ app/helpers/application_form.rb
@@ -1,12 +1,21 @@
 class ApplicationForm < ActionView::Helpers::FormBuilder
-  def initialize(object_name, object, template, options)
-    classes = 'bg-blue-400 p-4'
+  def initialize(object_name, object, template, options = {})
+    classes = 'bg-blue-400 flex flex-col'
 
     options[:class] = classes if options[:class].blank?
+
     super
   end
 
-  def submit(value = nil, options = {})
+  def text_field(value, options = {})
+    classes = 'bg-green-400 flex flex-col m-2'
+
+    options[:class] = classes if options[:class].blank?
+
+    super
+  end
+
+  def submit(value, options = {})
     classes = 'bg-red-300 p-4'
 
     options[:class] = classes if options[:class].blank?
