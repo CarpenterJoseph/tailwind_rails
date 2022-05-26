d4c88b9e9aa0ffbbf48d8bfec9b4fdf90145681f
Joseph Carpenter <carpenter.joseph1998@gmail.com>
2022-05-26 17:50:00 +0200

    move add classes to its own function

--- app/helpers/application_form.rb
+++ app/helpers/application_form.rb
@@ -1,8 +1,8 @@
 class ApplicationForm < ActionView::Helpers::FormBuilder
   def initialize(object_name, object, template, options = {})
-    classes = 'bg-blue-400 flex flex-col'
+    classes = 'flex flex-col m-1'
 
-    options[:class] = classes if options[:class].blank?
+    add_classes(options, classes)
 
     super
   end
@@ -10,16 +10,22 @@ class ApplicationForm < ActionView::Helpers::FormBuilder
   def text_field(value, options = {})
     classes = 'bg-green-400 flex flex-col m-2'
 
-    options[:class] = classes if options[:class].blank?
+    add_classes(options, classes)
 
     super
   end
 
   def submit(value, options = {})
-    classes = 'bg-red-300 p-4'
+    classes = 'bg-red-300 p-4 self-end m-2'
 
-    options[:class] = classes if options[:class].blank?
+    add_classes(options, classes)
 
     super
+  end
+
+  private
+
+  def add_classes(options = {}, classes)
+    options[:class] = classes if options[:class].blank?
   end
 end
